22955f5b4e30f98872d2bdeb9eb730e8b045799a
Isaac Cruz <isaac.clm1@gmail.com>
2021-12-09 19:47:53 -0300

    ADD: Initial html template and API request

--- renderer.js
+++ renderer.js
@@ -4,3 +4,17 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+function getWheaterByCityId() {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=0323a0a2e456757e8a98c09bec2ba751';
+    const request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send();
+    return request.responseText
+}
+
+function main() {
+    console.log(getWheaterByCityId());
+}
+
+main();
